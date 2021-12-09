341574272941066bfa1757646e0974f6e07395cb
Yusuf İşleyen <me@yusufisleyen.com>
2021-12-09 22:43:09 +0300

    TC No Generate

---
+++ generate.go
@@ -0,0 +1,46 @@
+package tckimlik
+
+import (
+	"math/rand"
+	"strconv"
+)
+
+var (
+	digits [11]int
+	tcIdentity string
+	evenNumbersTotal int
+	oddNumbersTotal int
+)
+
+func Generate () string  {
+	tcIdentity = ""
+
+	// generate the first number (1 - 10)
+	digits[0] = rand.Intn(10-1) + 1
+
+	// generate other numbers (2-3-4-5-6-7-8)
+	for i:= 1; i<9; i++ {
+		digits[i] = rand.Intn(10-0) + 0
+	}
+
+	// Calculate the sum of even and odd numbers
+	for i:= 1; i<9; i++ {
+		if i%2==0 {
+			evenNumbersTotal += digits[i]
+		} else {
+			oddNumbersTotal += digits[i]
+		}
+	}
+
+	// the tenth digit is calculated
+	digits[9] = (evenNumbersTotal*7 - oddNumbersTotal) % 10
+
+	// the eleventh digit is calculated
+	digits[10] = (evenNumbersTotal + oddNumbersTotal + digits[9]) % 10
+
+	for _, digit := range digits {
+		tcIdentity += strconv.Itoa(digit)
+	}
+
+	return tcIdentity
+}
